ecaa5cad754f8a6671ece382228afaa62b48cdbb
Pedro Vasconcellos <pedronvas18@gmail.com>
2018-03-20 21:42:01 -0300

    ListContacts component

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,10 +1,65 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import ListContacts from './ListContacts'
 
+/* Updating state
+
+You should you a function in setState when the current state depends on the previous one
+
+this.setState((prevState) => ({
+	count: prevState.count + 1
+}))
+
+You should use an object whenever else
+this.setState({
+	username: 'Tyler'
+})
+
+ */
+
+/*
+ DON'T EVER USE PROPS WHEN INITIATING A STATE
+
+ this.state = {
+    user: props.user
+ }
+
+ */
 class App extends Component {
+	state = {
+		contacts: [
+			{
+				"id": "ryan",
+				"name": "Ryan Florence",
+				"email": "ryan@reacttraining.com",
+				"avatarURL": "http://localhost:5001/ryan.jpg"
+			},
+			{
+				"id": "michael",
+				"name": "Michael Jackson",
+				"email": "michael@reacttraining.com",
+				"avatarURL": "http://localhost:5001/michael.jpg"
+			},
+			{
+				"id": "tyler",
+				"name": "Tyler McGinnis",
+				"email": "tyler@reacttraining.com",
+				"avatarURL": "http://localhost:5001/tyler.jpg"
+			}
+		]
+	};
+
+	removeContact = (contact) => {
+		this.setState((state) => ({
+			contacts: state.contacts.filter((c) => c.id !== contact.id)
+		}))
+	};
+
 	render() {
 		return (
 			<div>
-				Hello World
+				<ListContacts
+					onDeleteContact={this.removeContact}
+					contacts={this.state.contacts}/>
 			</div>
 		)
 	}
---
+++ contacts/src/ListContacts.js
@@ -0,0 +1,126 @@
+import escapeRegExp from 'escape-string-regexp'
+import PropTypes from 'prop-types'
+import React, {Component} from 'react'
+import sortBy from 'sort-by'
+
+/* Stateless Functional Component
+
+If your component only has a render method it can actually be just a function.
+instead of 'class ListContacts extends Component
+
+* Takes in props as the first (and only) argument
+* Returns description of UI
+* No 'this' keyword
+
+function ListContacts(props) {
+	return (
+		<ol className='contact-list'>
+			{props.contacts.map(contact => (
+				<li key={contact.id} className='contact-list-item'>
+					<div className='contact-avatar' style={{
+						backgroundImage: `url(${contact.avatarURL})`
+					}}/>
+					<div className='contact-details'>
+						<p>{contact.name}</p>
+						<p>{contact.email}</p>
+					</div>
+					<button onClick={() => props.onDeleteContact(contact)} className='contact-remove'>
+						Remove
+					</button>
+				</li>
+			))}
+		</ol>
+	)
+}
+
+ListContacts.propTypes = {
+	contacts: PropTypes.array.isRequired,
+	onDeleteContact: PropTypes.func.isRequired
+};
+
+*/
+
+// const ListExample = (props) => (
+// 	<div>
+// 		{props.text}
+// 	</div>
+// );
+
+class ListContacts extends Component {
+
+	// PropTypes can be an attribute (make it static)
+	static propTypes = {
+		contacts: PropTypes.array.isRequired,
+		onDeleteContact: PropTypes.func.isRequired
+	};
+
+	state = {
+		query: ''
+	};
+
+	updateQuery = (query) => {
+		this.setState({query: query.trim()})
+	};
+
+	clearQuery = () => {
+		this.setState({query: ''})
+	};
+
+	render() {
+		const {contacts, onDeleteContact} = this.props;
+		const {query} = this.state;
+
+		let showingContacts;
+
+		if (query) {
+			const match = new RegExp(escapeRegExp(query), 'i');
+			showingContacts = contacts.filter(contact => match.test(contact.name))
+		} else {
+			showingContacts = contacts
+		}
+
+		showingContacts.sort(sortBy('name'));
+
+		return (
+			<div className='list-contacts'>
+				<div className='list-contacts-top'>
+					<input
+						className='search-contacts'
+						type='text'
+						placeholder='Search contacts'
+						value={query}
+						onChange={(event) => this.updateQuery(event.target.value)}
+					/>
+				</div>
+
+				{showingContacts.length !== contacts.length && (
+					<div className='showing-contacts'>
+						<span>Now showing {showingContacts.length} of {contacts.length} total</span>
+						<button onClick={this.clearQuery}>Show all</button>
+					</div>
+				)}
+
+				<ol className='contact-list'>
+					{showingContacts.map(contact => (
+						<li key={contact.id} className='contact-list-item'>
+							<div className='contact-avatar' style={{
+								backgroundImage: `url(${contact.avatarURL})`
+							}}/>
+							<div className='contact-details'>
+								<p>{contact.name}</p>
+								<p>{contact.email}</p>
+							</div>
+							<button onClick={() => onDeleteContact(contact)} className='contact-remove'>
+								Remove
+							</button>
+						</li>
+					))}
+				</ol>
+			</div>
+		)
+	}
+
+}
+
+
+export default ListContacts
